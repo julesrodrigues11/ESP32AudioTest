5bce92d71299de306fc4944e5d0b52f1226cb233
Jules Rodrigues <jules.rodrigues11@gmail.com>
2024-05-09 12:21:46 +0100

    Added code for data receipt

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <SD.h>
 #include "AudioTools.h"
 #include "AudioCodecs/CodecWAV.h"
+#include "Wire.h"
 
 const int chipSelect = D9;
 I2SStream i2s; // final output of decoded stream
@@ -9,9 +10,25 @@ EncodedAudioStream decoder(&i2s, new WAVDecoder()); // Decoding stream
 StreamCopy copier; 
 File audioFile;
 
+#define SLAVE_ADDRESS 8
+
+// Function to Receive Data and act based on input received
+void receiveSignal(int numBytes)
+{
+  int c = Wire.read();
+  if (c < 30)
+  {
+    Serial.print("Below threshold - ");
+  }
+  Serial.println(c);
+}
+
 void setup(){
   Serial.begin(115200);
-  AudioLogger::instance().begin(Serial, AudioLogger::Info);  
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveSignal);
 
   // setup file
   SD.begin(chipSelect);
